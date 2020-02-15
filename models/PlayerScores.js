d6f60514d487e078cc58b484789f5654f5cf143d
ANCIENTDOOM <ancientdoomgaming@gmail.com>
2020-02-15 18:03:59 +0100

    Force kanji spawn. Add Player Scores

---
+++ models/PlayerScores.js
@@ -0,0 +1,7 @@
+module.exports = class PlayerScores {
+
+    constructor() {
+        this.score = 0;
+        this.kanji_caught = 0;
+    }
+}
--- utils/KanjiUtils.js
+++ utils/KanjiUtils.js
@@ -3,4 +3,8 @@ module.exports = class KanjiUtils {
     randomKanji(kanjis) {
         return kanjis[Math.floor(Math.random() * Math.floor(kanjis.length))];
     }
+
+    isValidGrade(grade){
+        return (grade > 0 && grade <= 6 && !isNaN(grade))
+    }
 }
